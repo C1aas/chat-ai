b4b70f8b124815b57e3851831754060abdde17cd
Claas Kochanke <claas.kochanke@gmail.com>
2025-08-26 11:42:04 +0200

    improved Footer

--- front/src/components/Footer/VersionDisplay.jsx
+++ front/src/components/Footer/VersionDisplay.jsx
@@ -28,7 +28,6 @@ export default function VersionDisplay() {
             <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
             </svg>
         </a>{" "}
-        | © 2025 GWDG | <Trans i18nKey="description.copyright"></Trans>
         </p>
     </div>
     );
